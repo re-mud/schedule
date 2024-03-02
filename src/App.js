c4f7979b5b0dd039fe09bb18bafd56946f911be2
remud <killersan0010@gmail.com>
2024-03-03 00:00:26 +0500

    change App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { useState } from 'react';
+import { Component } from 'react';
+import { lazy } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+const Main = lazy(() => import(`./components/Main/Main.js`));
+
+class App extends Component {
+    state = {
+        page: 'main',
+    };
+
+    getPage(namePage) {
+        switch (namePage) {
+            case 'main': return Main;
+        }
+    }
+
+    changePage(page) {
+        this.setState(page);
+    }
+
+    render() {
+        let Page = this.getPage(this.state.page);
+
+        return (
+            <Page />
+        );
+    }
 }
 
 export default App;
